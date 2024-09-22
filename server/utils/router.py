93703902a171b6de6fc633e6d534ec1dcdaf39b9
No767 <73260931+No767@users.noreply.github.com>
2024-09-22 16:42:24 -0700

    finally finish the implementation

--- server/utils/router.py
+++ server/utils/router.py
@@ -1,28 +1,20 @@
 from __future__ import annotations
 
-from fastapi import APIRouter
-
-from slowapi import Limiter, _rate_limit_exceeded_handler
-from slowapi.util import get_remote_address
-from slowapi.middleware import SlowAPIMiddleware
-from slowapi.errors import RateLimitExceeded
-from typing import TYPE_CHECKING
 from pathlib import Path
+
+from fastapi import APIRouter
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+
 from .config import KanaeConfig
 
-# There isn't really a good solution to this except to do this
-# I really hate doing this but oh well
-config_path = Path(__file__).parents[1] / "config.yml"
-config = KanaeConfig(config_path)
+CONFIG_PATH = Path(__file__).parents[1] / "config.yml"
+
 
 class KanaeRouter(APIRouter):
     limiter: Limiter
-    
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.limiter = Limiter(key_func=get_remote_address, storage_uri=config["redis_uri"])
-        
-        
-        
-        
-    
+        self._redis_uri = KanaeConfig(CONFIG_PATH)["redis_uri"]
+        self.limiter = Limiter(key_func=get_remote_address, storage_uri=self._redis_uri)
