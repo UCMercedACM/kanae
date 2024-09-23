152d9ab3861484460081116dcccec4480916a9ea
Noelle Wang <73260931+No767@users.noreply.github.com>
2024-09-22 17:22:58 -0700

    Implement ratelimits (#10)

--- server/launcher.py
+++ server/launcher.py
@@ -6,6 +6,8 @@ from pathlib import Path
 import uvicorn
 from core import Kanae
 from routes import router
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
 from utils.config import KanaeConfig
 from uvicorn.supervisors import Multiprocess
 
@@ -14,6 +16,9 @@ config = KanaeConfig(config_path)
 
 app = Kanae(config=config)
 app.include_router(router)
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)  # type: ignore
+app.state.limiter = router.limiter
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -1,9 +1,9 @@
 import importlib
 from pkgutil import iter_modules
 
-from fastapi import APIRouter
+from utils.router import KanaeRouter
 
-router = APIRouter()
+router = KanaeRouter()
 route_modules = [module.name for module in iter_modules(__path__, f"{__package__}.")]
 
 for route in route_modules:
