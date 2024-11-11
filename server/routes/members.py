151cc35c6f4cbe81b3aa131c3eb9efd352db724c
A13xYanez <145167791+A13xYanez@users.noreply.github.com>
2024-11-11 15:28:53 -0800

    create a draft for the get members endpoint

--- server/routes/members.py
+++ server/routes/members.py
@@ -1,5 +1,36 @@
 from pydantic import BaseModel
 from utils.request import RouteRequest
 from utils.router import KanaeRouter
+from utils.errors import NotFoundException
+import uuid
 
 router = KanaeRouter(tags=["Members"])
+
+
+
+class Members(BaseModel):
+    name: str
+
+
+
+@router.get("/members/{id}", name="Get Members")
+async def get_members(request: RouteRequest, id: uuid.UUID):
+    query = """
+            SELECT name
+            FROM members
+            """
+    
+    members = await request.app.pool.fetchrow(query, id)
+
+    if not members:
+        raise NotFoundException()
+    
+    return Members(**dict(members))
+
+
+
+# TODO: Update member by id
+
+
+
+# TODO: Delete member by id
