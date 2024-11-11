6e482fccc64a9e91ab8f5b9bfeb7805b85466671
No767 <73260931+No767@users.noreply.github.com>
2024-11-11 00:45:25 -0800

    merge

--- server/routes/events.py
+++ server/routes/events.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
 from utils.request import RouteRequest
 from utils.router import KanaeRouter
-from utils.errors import NotFoundMessage
 from typing import Optional, Literal
+from utils.errors import NotFoundException
 import datetime
+import uuid
 
 router = KanaeRouter(tags=["Events"])
 
@@ -27,11 +28,8 @@ class Events(BaseModel):
     ]
 
 
-@router.get(
-    "/events",
-    responses={200: {"model": Events}, 404: {"model": NotFoundMessage}},
-    name="List events",
-)
+# TODO: Ensure that the limits for how much can be requested is in place
+@router.get("/events", name="List events")
 async def events_list(request: RouteRequest) -> list[Events]:
     query = """
     SELECT name, description, start_at, end_at, location, alt_link, type
@@ -42,6 +40,7 @@ async def events_list(request: RouteRequest) -> list[Events]:
     return [Events(**record) for record in rows]
 
 
+# TODO: Enforce status codes if the event is unique, etc
 @router.post("/events/create", name="Create Events")
 async def events_create(request: RouteRequest, req: Events) -> Events:
     query = """
@@ -51,3 +50,17 @@ async def events_create(request: RouteRequest, req: Events) -> Events:
     event_req = req.model_dump()
     await request.app.pool.execute(query, *event_req.values())
     return req
+
+
+@router.get("/events/{id}", name="Get event", responses={200: {"model": Events}})
+async def get_event(request: RouteRequest, id: uuid.UUID) -> Events:
+    query = """
+    SELECT name, description, start_at, end_at, location, alt_link, type
+    FROM events
+    WHERE id = $1;
+    """
+
+    rows = await request.app.pool.fetchrow(query, id)
+    if not rows:
+        raise NotFoundException()
+    return Events(**dict(rows))
