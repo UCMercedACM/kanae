42b393f88c9b04d2eb5f04e0cd9f8af9de308cda
No767 <73260931+No767@users.noreply.github.com>
2024-11-16 23:50:30 -0800

    work on events endpoints

--- server/routes/events.py
+++ server/routes/events.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel
+from typing import Annotated
 from utils.request import RouteRequest
 from utils.router import KanaeRouter
 from typing import Optional, Literal
-from utils.errors import NotFoundException
+from utils.errors import NotFoundException, NotFoundMessage
+from fastapi import Query
 import datetime
 import uuid
 
@@ -15,7 +17,6 @@ class Events(BaseModel):
     start_at: datetime.datetime
     end_at: datetime.datetime
     location: str
-    alt_link: Optional[str] = None
     type: Literal[
         "general",
         "sig_ai",
@@ -28,39 +29,63 @@ class Events(BaseModel):
     ]
 
 
-# TODO: Ensure that the limits for how much can be requested is in place
-@router.get("/events", name="List events")
-async def events_list(request: RouteRequest) -> list[Events]:
+# TODO: Pagination the responses
+@router.get("/events")
+async def list_events(
+    request: RouteRequest,
+    name: Annotated[Optional[str], Query(min_length=3)] = None,
+    limit: Annotated[int, Query(gt=0, le=100)] = 50,
+) -> list[Events]:
     query = """
-    SELECT name, description, start_at, end_at, location, alt_link, type
+    SELECT id, name, description, start_at, end_at, location, type
     FROM events
-    WHERE created_at >= (NOW() - INTERVAL '12 hours');
+    ORDER BY start_at DESC
+    LIMIT $1;
     """
-    rows = await request.app.pool.fetch(query)
+
+    if name:
+        query = """
+        SELECT id, name, description, start_at, end_at, location, type
+        FROM events
+        WHERE name % $1
+        ORDER BY similarity(name, $1) DESC
+        LIMIT 10;
+        """
+
+    arg = name if name else limit
+    rows = await request.app.pool.fetch(query, arg)
+
     return [Events(**record) for record in rows]
 
 
-# TODO: Enforce status codes if the event is unique, etc
-@router.post("/events/create", name="Create Events")
-async def events_create(request: RouteRequest, req: Events) -> Events:
-    query = """
-    INSERT INTO events (name, description, start_at, end_at, location, alt_link, type)
-    VALUES ($1, $2, $3, $4, $5, $6, $7);
-    """
-    event_req = req.model_dump()
-    await request.app.pool.execute(query, *event_req.values())
-    return req
+class EventsWithID(Events):
+    id: uuid.UUID
 
 
-@router.get("/events/{id}", name="Get event", responses={200: {"model": Events}})
+@router.get(
+    "/events/{id}",
+    responses={200: {"model": EventsWithID}, 404: {"model": NotFoundMessage}},
+)
 async def get_event(request: RouteRequest, id: uuid.UUID) -> Events:
     query = """
-    SELECT name, description, start_at, end_at, location, alt_link, type
+    SELECT name, description, start_at, end_at, location, type
     FROM events
     WHERE id = $1;
     """
 
     rows = await request.app.pool.fetchrow(query, id)
     if not rows:
-        raise NotFoundException()
+        raise NotFoundException
     return Events(**dict(rows))
+
+
+# TODO: Enforce status codes if the event is unique, etc
+@router.post("/events/create")
+async def create_events(request: RouteRequest, req: Events) -> Events:
+    query = """
+    INSERT INTO events (name, description, start_at, end_at, location, type)
+    VALUES ($1, $2, $3, $4, $5, $6);
+    """
+    event_req = req.model_dump()
+    await request.app.pool.execute(query, *event_req.values())
+    return req
