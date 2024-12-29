ae2e208a2ea3f095d8618042457b356ad12c04ce
No767 <73260931+No767@users.noreply.github.com>
2024-12-28 16:59:42 -0800

    finish listing projects

--- server/routes/projects.py
+++ server/routes/projects.py
@@ -2,64 +2,81 @@ import datetime
 import uuid
 from typing import Annotated, Literal, Optional
 
-from fastapi import Query
+from fastapi import Depends, Query
 from pydantic import BaseModel
 from utils.errors import BadRequestException
+from utils.pages import KanaePages, KanaeParams, paginate
 from utils.request import RouteRequest
 from utils.router import KanaeRouter
 
 router = KanaeRouter(tags=["Projects"])
 
+
 class ProjectMembers(BaseModel, frozen=True):
     id: uuid.UUID
     name: str
+
+
 class Projects(BaseModel):
     id: uuid.UUID
     name: str
     description: str
     link: str
     members: list[ProjectMembers]
-    type: Literal["independent", "sig_ai", "sig_swe", "sig_cyber", "sig_data", "sig_arch", "sig_graph"]
+    type: Literal[
+        "independent",
+        "sig_ai",
+        "sig_swe",
+        "sig_cyber",
+        "sig_data",
+        "sig_arch",
+        "sig_graph",
+    ]
     active: bool
     founded_at: datetime.datetime
 
-def _inject_args(since: Optional[datetime.datetime], until: Optional[datetime.datetime], active: Optional[bool], args: list) -> list:
-    if since:
-        args.extend((since, active))
-    elif until:
-        args.extend((until, active))
-    return args
-    
-#  name: Annotated[Optional[str], Query(min_length=3)], since: Optional[datetime.datetime], until: Optional[datetime.datetime]
+
 @router.get("/projects")
-async def list_projects(request: RouteRequest, name: Annotated[Optional[str], Query(min_length=3)] = None, since: Optional[datetime.datetime] = None, until: Optional[datetime.datetime] = None, active: Optional[bool] = True):
+async def list_projects(
+    request: RouteRequest,
+    name: Annotated[Optional[str], Query(min_length=3)] = None,
+    since: Optional[datetime.datetime] = None,
+    until: Optional[datetime.datetime] = None,
+    active: Optional[bool] = True,
+    *,
+    params: Annotated[KanaeParams, Depends()],
+) -> KanaePages[Projects]:
     if since and until:
-        raise BadRequestException("Cannot specify both parameters. Must be only one be specified.")
-    
-    constraint = ""
+        raise BadRequestException(
+            "Cannot specify both parameters. Must be only one be specified."
+        )
+
     args = []
-    
-    # There is probably a more optimized way of doing this - Noelle
-    if name and (since or until):
-        time_constraint = ""
-        args.append(name)
-        if since:
-            time_constraint = "projects.founded_at >= $2"
-            args.extend((since, active))
-        elif until:
-            time_constraint = "projects.founded_at <= $2"
-            args.extend((until, active))
-        constraint = f"WHERE projects.name % $1 AND  {time_constraint} AND projects.active = $3 GROUP BY projects.id ORDER BY similarity(projects.name, $1) DESC"
+    time_constraint = ""
+
+    if name:
+        if since or until:
+            if since:
+                time_constraint = "AND projects.founded_at >= $2"
+                args.append(since)
+            elif until:
+                time_constraint = "AND projects.founded_at <= $2"
+                args.append(until)
+
+        constraint = f"WHERE projects.name % $1 {time_constraint} GROUP BY projects.id ORDER BY similarity(projects.name, $1) DESC"
+        args.insert(0, name)
+    elif active is not None:
+        constraint = "WHERE projects.active = $1 GROUP BY projects.id"
+        args.append(active)
     else:
         if since:
-            constraint = "WHERE projects.founded_at >= $1 AND projects.active = $2 GROUP BY projects.id"
+            time_constraint = "projects.founded_at >= $1 AND projects.active = $2"
             args.extend((since, active))
         elif until:
-            constraint = "WHERE projects.founded_at <= $1 AND projects.active = $2 GROUP BY projects.id"
+            time_constraint = "projects.founded_at <= $1 AND projects.active = $2"
             args.extend((until, active))
-        
+        constraint = f"WHERE {time_constraint} GROUP BY projects.id"
 
-        
     query = f"""
     SELECT 
         projects.id, projects.name, projects.description, projects.link, 
@@ -68,15 +85,7 @@ async def list_projects(request: RouteRequest, name: Annotated[Optional[str], Qu
     FROM projects
     LEFT OUTER JOIN project_members ON project_members.project_id = projects.id
     LEFT OUTER JOIN members ON project_members.member_id = members.id
-    {constraint};
+    {constraint}
     """
 
-    print(query)
-    print(args)
-        
-    
-        
-        
-    
-    records = await request.app.pool.fetch(query, *args)
-    return [Projects(**dict(project)) for project in records]
+    return await paginate(request.app.pool, query, *args, params=params)
