7d153fec77b31b44519851209cb2f7307b5a9695
No767 <73260931+No767@users.noreply.github.com>
2025-03-15 20:02:12 -0700

    some additional changes

--- server/routes/members.py
+++ server/routes/members.py
@@ -1,5 +1,7 @@
 import uuid
 
+from events import EventsWithID
+from projects import PartialProjects
 from pydantic import BaseModel
 from utils.errors import NotFoundException
 from utils.request import RouteRequest
@@ -8,25 +10,25 @@ from utils.router import KanaeRouter
 router = KanaeRouter(tags=["Members"])
 
 
+class PartialMember(BaseModel):
+    id: str
+    name: str
+
+
+class Member(BaseModel):
+    name: str
+    email: str
+    projects: list[PartialProjects]
+    events: list[EventsWithID]
+
+
 class Members(BaseModel):
     name: str
 
 
-# Endpoint for getting members by their id
-@router.get("/members/{id}", name="Get Members")
-async def get_members(request: RouteRequest, id: uuid.UUID) -> Members:
-    query = """
-            SELECT name
-            FROM members
-            WHERE id = $1
-            """
-
-    member = await request.app.pool.fetchrow(query, id)
-
-    if not member:
-        raise NotFoundException()
-
-    return Members(**dict(member))
+# TODO: Figure out an more efficent way to join between the M to M tables in one go
+# @router.get("/members/{id}")
+# async def get_member(request: RouteRequest, id: uuid.UUID) -> None:
 
 
 # Endpoint for deleting members by thier id
