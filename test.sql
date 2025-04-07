273888f943c22864903d12522364ac6f0e1e98fc
No767 <73260931+No767@users.noreply.github.com>
2025-04-07 00:30:09 -0700

    merge

--- test.sql
+++ test.sql
@@ -1,10 +1,6 @@
 SELECT members.id,
-    members.name,
-    jsonb_agg(to_jsonb(row(projects.id, projects.name))) AS user_projects,
-    members.created_at
+    (SELECT jsonb_agg(json_build_object('id', projects.id, 'name', projects.name)) FROM projects WHERE projects.id = project_members.project_id) AS member_projects
 FROM members
     INNER JOIN project_members ON members.id = project_members.member_id
     INNER JOIN events_members ON members.id = events_members.member_id
-    INNER JOIN projects ON projects.id = project_members.project_id
-    INNER JOIN events ON events.id = events_members.event_id
 GROUP BY members.id;
