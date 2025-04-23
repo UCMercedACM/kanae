a4eddbbf430d4b19e749b004af20e27c9a6d34b9
No767 <73260931+No767@users.noreply.github.com>
2025-04-22 23:59:04 -0700

    finish members endpoints

--- server/routes/members.py
+++ server/routes/members.py
@@ -1,12 +1,41 @@
 import datetime
 import uuid
-from typing import Annotated, Literal, Optional
+from typing import Annotated, Literal, Optional, Union
 
+import asyncpg
+from email_validator import EmailNotValidError, validate_email
 from fastapi import Depends
 from pydantic import BaseModel
+from supertokens_python.asyncio import get_user, list_users_by_account_info
+from supertokens_python.recipe.accountlinking.asyncio import is_email_change_allowed
+from supertokens_python.recipe.emailpassword.asyncio import (
+    update_email_or_password,
+    verify_credentials,
+)
+from supertokens_python.recipe.emailpassword.interfaces import (
+    PasswordPolicyViolationError,
+    WrongCredentialsError,
+)
+from supertokens_python.recipe.emailverification.asyncio import (
+    is_email_verified,
+    send_email_verification_email,
+)
+from supertokens_python.recipe.passwordless.asyncio import update_user
+from supertokens_python.recipe.passwordless.interfaces import (
+    UpdateUserEmailAlreadyExistsError,
+    UpdateUserOkResult,
+)
 from supertokens_python.recipe.session import SessionContainer
+from supertokens_python.recipe.session.asyncio import revoke_all_sessions_for_user
 from supertokens_python.recipe.session.framework.fastapi import verify_session
-from utils.errors import NotFoundException, NotFoundMessage
+from supertokens_python.types import AccountInfo
+from utils.errors import (
+    BadRequestException,
+    HTTPException,
+    HTTPExceptionMessage,
+    NotFoundException,
+    NotFoundMessage,
+)
 from utils.request import RouteRequest
 from utils.router import KanaeRouter
 
@@ -59,15 +88,9 @@ class ClientMember(BaseModel, frozen=True):
     events: list[ClientEvents]
 
 
-@router.get(
-    "/members/me",
-    responses={200: {"model": ClientMember}, 404: {"model": NotFoundMessage}},
-)
-async def get_logged_member(
-    request: RouteRequest,
-    session: Annotated[SessionContainer, Depends(verify_session())],
+async def get_member_info(
+    id: Union[str, uuid.UUID], *, pool: asyncpg.Pool
 ) -> ClientMember:
-    """Obtain details pertaining to the currently authenticated user"""
     query = """
     SELECT members.id,
         members.name,
@@ -101,10 +124,31 @@ async def get_logged_member(
     WHERE members.id = $1
     GROUP BY members.id;
     """
-    rows = await request.app.pool.fetchrow(query, session.get_user_id())
+    rows = await pool.fetchrow(query, id)
     if not rows:
         raise NotFoundException
     return ClientMember(**(dict(rows)))
+
+
+@router.get(
+    "/members/me",
+    responses={200: {"model": ClientMember}, 404: {"model": NotFoundMessage}},
+)
+async def get_logged_member(
+    request: RouteRequest,
+    session: Annotated[SessionContainer, Depends(verify_session())],
+) -> ClientMember:
+    """Obtain details pertaining to the currently authenticated user"""
+    return await get_member_info(session.get_user_id(), pool=request.app.pool)
+
+
+@router.get(
+    "/members/{id}",
+    responses={200: {"model": ClientMember}, 404: {"model": NotFoundMessage}},
+)
+async def get_member(request: RouteRequest, id: uuid.UUID) -> ClientMember:
+    """Obtain details pertaining to the specified user"""
+    return await get_member_info(id, pool=request.app.pool)
 
 
 @router.get("/members/me/projects", responses={200: {"model": ClientProjects}})
@@ -183,4 +227,132 @@ async def get_logged_events(
 class ModifiedClient(BaseModel, frozen=True):
     name: Optional[str] = None
     email: Optional[str] = None
-    password: Optional[str] = None
+    old_password: Optional[str] = None
+    new_password: Optional[str] = None
+
+
+class SuccessResponse(BaseModel, frozen=True):
+    message: str
+
+
+@router.put(
+    "/members/me/update",
+    responses={
+        200: {"model": SuccessResponse},
+        400: {"model": HTTPExceptionMessage},
+        401: {"model": HTTPExceptionMessage},
+        404: {"model": NotFoundMessage},
+        409: {"model": HTTPExceptionMessage},
+    },
+)
+async def update_logged_member(
+    request: RouteRequest,
+    req: ModifiedClient,
+    session: Annotated[SessionContainer, Depends(verify_session())],
+) -> SuccessResponse:
+    """Updates information for the currently authenticated member"""
+    member_info = await get_user(session.get_user_id())
+
+    if not member_info:
+        raise NotFoundException
+
+    if req.old_password and req.new_password:
+        login_method = next(
+            (
+                method
+                for method in member_info.login_methods
+                if method.recipe_user_id.get_as_string() == session.get_recipe_user_id()
+                and method.recipe_id == "emailpassword"
+            ),
+            None,
+        )
+
+        if login_method and login_method.email:
+            is_valid_password = await verify_credentials(
+                "public", login_method.email, req.old_password
+            )
+            if isinstance(is_valid_password, WrongCredentialsError):
+                raise HTTPException(
+                    status_code=401, detail="Wrong credentials provided"
+                )
+
+            response = await update_email_or_password(
+                session.get_recipe_user_id(),
+                password=req.new_password,
+                tenant_id_for_password_policy=session.get_tenant_id(),
+            )
+            if isinstance(response, PasswordPolicyViolationError):
+                raise HTTPException(
+                    status_code=409,
+                    detail="Password conflicts with current password policy",
+                )
+
+            await revoke_all_sessions_for_user(session.get_user_id())
+            await session.revoke_session()
+
+            return SuccessResponse(
+                message="Password successfully changed. Log in again to use your new password"
+            )
+
+        raise NotFoundException(detail="No email found or invalid login method used")
+
+    elif req.name:
+        query = "UPDATE members SET name = $2 WHERE id = $1"
+        await request.app.pool.execute(query, session.get_user_id(), req.name)
+        return SuccessResponse(message="Name successfully changed")
+
+    elif req.email:
+        is_valid_email = validate_email(req.email, check_deliverability=True)
+        if isinstance(is_valid_email, EmailNotValidError):
+            raise BadRequestException("Email is not valid")
+
+        normalized_email = is_valid_email.normalized
+        is_verified = is_email_verified(session.get_recipe_user_id(), normalized_email)
+
+        if not is_verified:
+            if not is_email_change_allowed(
+                session.get_recipe_user_id(), normalized_email, False
+            ):
+                raise BadRequestException("Email change not allowed")
+
+            member = await get_user(session.get_user_id())
+
+            if member:
+                for tenant_id in member.tenant_ids:
+                    members_with_same_email = await list_users_by_account_info(
+                        tenant_id, AccountInfo(email=normalized_email)
+                    )
+                    for curr_member in members_with_same_email:
+                        if curr_member.id != session.get_user_id():
+                            raise HTTPException(
+                                status_code=409,
+                                detail="Requested email conflicts with other members",
+                            )
+
+                await send_email_verification_email(
+                    session.get_tenant_id(),
+                    session.get_user_id(),
+                    session.get_recipe_user_id(),
+                    normalized_email,
+                )
+                return SuccessResponse(
+                    message="Sent email verification message. Please check your email and spam folder"
+                )
+
+        response = await update_user(
+            session.get_recipe_user_id(), email=normalized_email
+        )
+
+        if isinstance(response, UpdateUserOkResult):
+            query = "UPDATE members SET email = $2 WHERE id = $1;"
+            await request.app.pool.execute(
+                query, session.get_user_id(), normalized_email
+            )
+            return SuccessResponse(message="Successfully changed email")
+
+        if isinstance(response, UpdateUserEmailAlreadyExistsError):
+            raise HTTPException(
+                status_code=409, detail="Email already exists, try another one"
+            )
+
+    raise HTTPException(status_code=500, detail="How...")
