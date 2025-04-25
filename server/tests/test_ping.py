fe6420f83a9a3256c3f98ab80797ffcf4b43c60e
No767 <73260931+No767@users.noreply.github.com>
2025-04-24 21:04:21 -0700

    fix?

--- server/tests/test_ping.py
+++ server/tests/test_ping.py
@@ -2,7 +2,7 @@ import pytest
 from conftest import KanaeTestClient
 
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio()
 async def test_ping(app: KanaeTestClient):
     response = await app.client.get("/docs")
     assert response.status_code == 200
