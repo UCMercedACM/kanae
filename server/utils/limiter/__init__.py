feece41ac79b06276d0166138e4cbc4925bc65fb
No767 <73260931+No767@users.noreply.github.com>
2025-08-04 01:06:34 +0900

    fix up import errors

--- server/utils/limiter/__init__.py
+++ server/utils/limiter/__init__.py
@@ -1,3 +1,7 @@
 """Internal package that rewrites Slowapi to be entirely async"""
 
 from .extension import KanaeLimiter as KanaeLimiter
+from .utils import (
+    get_ipaddr as get_ipaddr,
+    get_remote_address as get_remote_address,
+)
