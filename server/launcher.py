7b7187fb3eaf5d61c0dd7bda6cc38531024c93ea
No767 <73260931+No767@users.noreply.github.com>
2026-05-10 01:22:08 -0700

    switch to config-based logging

--- server/launcher.py
+++ server/launcher.py
@@ -1,11 +1,7 @@
-import os
-
 from core import Kanae, KanaeConfig, find_config
 from fastapi_pagination import add_pagination
 from routes import router
 from starlette.middleware.cors import CORSMiddleware
-from utils.uvicorn.config import KanaeUvicornConfig
-from utils.uvicorn.server import KanaeUvicornServer
 
 config = KanaeConfig.load_from_file(find_config())
 
@@ -24,16 +20,3 @@ app.limiter = router.limiter
 
 if app.is_prometheus_enabled:
     app.instrumentator.add_middleware()
-
-
-if __name__ == "__main__":
-    config = KanaeUvicornConfig(
-        "launcher:app",
-        port=config.kanae.port,
-        host=config.kanae.host,
-        workers=os.cpu_count() or 2,
-        access_log=True,
-    )
-
-    server = KanaeUvicornServer(config)
-    server.run()
