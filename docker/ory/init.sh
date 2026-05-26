9a24033d0fc5220d7c728557f52c6965370e6f89
No767 <73260931+No767@users.noreply.github.com>
2026-05-26 02:50:52 -0700

    Full integration testing

--- docker/ory/init.sh
+++ docker/ory/init.sh
@@ -2,8 +2,12 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE EXTENSION IF NOT EXISTS pg_trgm;
-
+    CREATE EXTENSION pg_trgm;
     CREATE DATABASE kratos;
     CREATE DATABASE keto;
+EOSQL
+
+# Apparently Atlas requires this for diff computation...
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
+    CREATE EXTENSION IF NOT EXISTS pg_trgm;
 EOSQL
